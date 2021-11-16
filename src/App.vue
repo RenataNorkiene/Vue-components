c804cd696a082190b4cbf59bcd2095dbb21b9b56
RenataNorkiene <renata.norkiene@gmail.com>
2021-11-16 21:49:02 +0200

    created main vue app with two child components and shared data with props ant emits

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <active-user :name="user.name" :age="user.age"></active-user>
+  <user-data @set-data="setUserData"></user-data>
+</div>
+</template>
+
+<script>
+import ActiveUser from "@/components/ActiveUser";
+import UserData from "@/components/UserData";
+export default {
+  name: "App",
+  components: {UserData, ActiveUser},
+  data() {
+    return {
+      user: {
+        name: 'Renata Norkiene',
+        age: 36,
+      }
+    };
+  },
+  methods: {
+    setUserData(name, age){
+      this.user = {
+        name: name,
+        age: +age,
+      };
+    }
+  }
+}
+</script>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2>{{ name }}</h2>
+    <h3>{{ age }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ActiveUser",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      require: true,
+    }
+  }
+};
+</script>
